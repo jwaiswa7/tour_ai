a090e0742eb77a991ee87876fdbcd9245ab207c9
Joshua Waiswa <jwaiswa7@gmail.com>
2024-05-29 14:31:02 +0300

    Add AI service

---
+++ app/services/chat_completion.rb
@@ -0,0 +1,21 @@
+class ChatCompletion < ApplicationService
+  def initialize
+    @client = OpenAI::Client.new
+  end
+
+  def call
+    response = client.chat(
+      parameters: {
+      model: "gpt-4o", # Required.
+      messages: [{ role: "user", content: "Hello!"}], # Required.
+      temperature: 0.7,
+      }
+    )
+
+    response = JSON.parse(response.to_json, object_class: OpenStruct)
+
+    response.choices[0].message.content
+  end
+
+  attr_accessor :client
+end
