4adb377c6efaa5c82bb58120712ddd52021e09ee
Joshua Waiswa <jwaiswa7@gmail.com>
2024-07-08 13:08:26 +0300

    Add vector store model

---
+++ app/services/ai/files.rb
@@ -0,0 +1,15 @@
+module Ai
+  class Files < Base
+    def call
+      client.files.list
+    end
+
+    private
+
+    def add_files
+      file = Rails.root.join('lib', 'assets', 'tour_data.json')
+      file = File.open(file, 'rb')
+      client.files.upload(parameters: { file: file, purpose: "assistants" })
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_08_091745) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_08_094921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_08_091745) do
     t.datetime "updated_at", null: false
     t.string "access_scopes", default: "", null: false
     t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  end
+
+  create_table "vector_stores", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.string "remote_id"
+    t.integer "file_counts"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
