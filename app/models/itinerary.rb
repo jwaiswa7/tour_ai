b2066d29c075f00ef577ca1a3f92f9c12d197140
Joshua Waiswa <jwaiswa7@gmail.com>
2024-08-08 13:04:21 +0300

    Add images and places to visit

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -16,19 +16,6 @@ class Itinerary < ApplicationRecord
     "Volunteering"
   ].freeze
 
-  DESTINATIONS = [
-    "Bwindi Impenetrable",
-    "Queen Elizabeth National park",
-    "Muchison Falls National park",
-    "Kibale National park",
-    "Lake Mburo National park",
-    "Kampala",
-    "Kidepo National park",
-    "Rwenzori mountains",
-    "Entebbe",
-    "Lake Bunyonyi"
-  ].freeze
-
   acts_as_taggable_on :interests, :destinations
 
   scope :recent, -> { order(created_at: :desc) }
@@ -40,6 +27,20 @@ class Itinerary < ApplicationRecord
   has_many :run_requests, dependent: :destroy
 
   after_create_commit { RequestAiJob.perform_async(id) }
+
+  def self.places_to_visit
+    file_path = Rails.root.join('lib', 'assets', 'places_to_visit.json')
+    if File.exist?(file_path)
+      file = File.read(file_path)
+      JSON.parse(file, symbolize_names: true).sample(6)
+    else
+      Rails.logger.error("File not found: #{file_path}")
+      []
+    end
+  rescue JSON::ParserError => e
+    Rails.logger.error("JSON parsing error: #{e.message}")
+    []
+  end
 
   def prompt
     "Create an itinerary for someone with the hobbies '#{interest_list.join(', ')}', they are interested in the destinations '#{destination_list.join(', ')}'  from #{start_date} to #{end_date}"
