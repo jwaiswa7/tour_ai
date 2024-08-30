79cf1c40f5648956ca0d7c6a7728c689d572a7af
Joshua Waiswa <jwaiswa7@gmail.com>
2024-08-30 13:31:11 +0300

    Submit the form using stimulus js

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,50 +1,49 @@
 import { Controller } from "@hotwired/stimulus";
-import { marked } from "marked";
 export default class extends Controller {
-  static targets = ["messages", "input", "question"]
+  static targets = ["messages", "input", "question", "thread"];
 
   connect() {
-    // set the height of the messages container to the height of the window
     this.messagesTarget.style.height = `${window.innerHeight - 100}px`
+    this.questionTarget.setAttribute("maxlength", 255);
   }
   
   submit(event) {
     event.preventDefault();
+    this.questionTarget.disabled = true;
+    this.messagesTarget.innerHTML += `<div class="answer-item m-5 shadow-lg p-4 rounded-lg bg-blue-500 text-white"><p>${this.questionTarget.value}</p></div>`;
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
+
     fetch('/questions.json', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
       },
-      body: JSON.stringify({ question: {question: this.questionTarget.value } })
+      body: JSON.stringify(
+        { 
+          question: {
+            question: this.questionTarget.value , 
+            thread_id: this.threadTarget.value  
+          } 
+        }
+      )
     }).then(response => {
-      if (response.ok) {
-        const form = event.target;
-        const formData = new FormData(form);
-
-        fetch(form.action, {
-          method: form.method,
-          headers: {
-            'Accept': 'application/json',
-            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-          },
-          body: formData
-        })
-        .then(response => response.json())
-        .then(data => {
-          // render text with turbo stream
-          const response = data.question.answer;
-          this.afterSubmit();
-        })
-        .catch(error => {
-          console.error("Error:", error);
-        });
-      }
-    })
+        if (response.ok) {
+          return response.json();
+        }
+    }).then(data => {
+      // render text with turbo stream
+      const response = data.question.answer;
+      this.messagesTarget.innerHTML += `<div class="answer-item m-5 shadow-lg p-4 rounded-lg"><p>${response}</p></div>`;
+      this.afterSubmit();
+    }).catch(error => {
+      console.error("Error:", error);
+    });
   }
 
   afterSubmit() {
-    this.inputTarget.value = "";
     this.questionTarget.value = "";
+    this.questionTarget.disabled = false;
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
   }
 }
