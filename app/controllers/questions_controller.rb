e428fdf32dc11cbe2a0656844f4a041d5e0e9cb4
Joshua Waiswa <jwaiswa7@gmail.com>
2024-08-30 12:35:38 +0300

    Updating the view to show activity

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,12 @@
 class QuestionsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
 
   end
 
   def create
-    ai_response = Ai::ChatBot::SendMessage.call(thread_id: question_params[:thread_id], message: question_params[:question])
+    # ai_response = Ai::ChatBot::SendMessage.call(thread_id: question_params[:thread_id], message: question_params[:question])
     @question = question_params[:question]
     @answer = ai_response["data"][0]["content"][0]["text"]["value"]
   end
@@ -16,9 +17,9 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:question, :thread_id)
   end
 
-  # def ai_response
-  #   {"object"=>"list", "data"=>[{"id"=>"msg_dmdqthtBgMKCbTPUQ36L3HRL", "object"=>"thread.message", "created_at"=>1724835708, "assistant_id"=>"asst_IP0QIGADUYeZXYWkB4xxX1Tl", "thread_id"=>"thread_YYt2zxvmaFfAIHP3ho9nksdm", "run_id"=>"run_ZEzWPfwf2BgYlbcDyRrE7QZx", "role"=>"assistant", "content"=>[{"type"=>"text", "text"=>{"value"=>"Great choice! Kenya is a beautiful country with a rich cultural heritage and stunning landscapes. Here are 5 popular tourist destinations in Kenya:\n\n1. **Maasai Mara National Reserve** – Famous for the Great Migration, wildlife viewing, and the Maasai culture.\n2. **Amboseli National Park** – Known for its stunning views of Mount Kilimanjaro and large elephant herds.\n3. **Lamu Island** – A serene and ancient Swahili town with beautiful beaches and historic architecture.\n4. **Lake Nakuru National Park** – Famous for its flocks of flamingos and diverse bird species.\n5. **Nairobi** – The capital city with attractions like the Nairobi National Park, David Sheldrick Wildlife Trust, and Karen Blixen Museum.\n\nPlease choose one or more destinations from the list above or feel free to enter your own preferred destinations.", "annotations"=>[]}}], "attachments"=>[], "metadata"=>{}}, {"id"=>"msg_CbA7QR8hdQ96DIQ1FXWbGSRR", "object"=>"thread.message", "created_at"=>1724835706, "assistant_id"=>nil, "thread_id"=>"thread_YYt2zxvmaFfAIHP3ho9nksdm", "run_id"=>nil, "role"=>"user", "content"=>[{"type"=>"text", "text"=>{"value"=>"I would like to visit Kenya", "annotations"=>[]}}], "attachments"=>[], "metadata"=>{}}], "first_id"=>"msg_dmdqthtBgMKCbTPUQ36L3HRL", "last_id"=>"msg_CbA7QR8hdQ96DIQ1FXWbGSRR", "has_more"=>false}
-  # end
+  def ai_response
+    {"object"=>"list", "data"=>[{"id"=>"msg_dmdqthtBgMKCbTPUQ36L3HRL", "object"=>"thread.message", "created_at"=>1724835708, "assistant_id"=>"asst_IP0QIGADUYeZXYWkB4xxX1Tl", "thread_id"=>"thread_YYt2zxvmaFfAIHP3ho9nksdm", "run_id"=>"run_ZEzWPfwf2BgYlbcDyRrE7QZx", "role"=>"assistant", "content"=>[{"type"=>"text", "text"=>{"value"=>"Great choice! Kenya is a beautiful country with a rich cultural heritage and stunning landscapes. Here are 5 popular tourist destinations in Kenya:\n\n1. **Maasai Mara National Reserve** – Famous for the Great Migration, wildlife viewing, and the Maasai culture.\n2. **Amboseli National Park** – Known for its stunning views of Mount Kilimanjaro and large elephant herds.\n3. **Lamu Island** – A serene and ancient Swahili town with beautiful beaches and historic architecture.\n4. **Lake Nakuru National Park** – Famous for its flocks of flamingos and diverse bird species.\n5. **Nairobi** – The capital city with attractions like the Nairobi National Park, David Sheldrick Wildlife Trust, and Karen Blixen Museum.\n\nPlease choose one or more destinations from the list above or feel free to enter your own preferred destinations.", "annotations"=>[]}}], "attachments"=>[], "metadata"=>{}}, {"id"=>"msg_CbA7QR8hdQ96DIQ1FXWbGSRR", "object"=>"thread.message", "created_at"=>1724835706, "assistant_id"=>nil, "thread_id"=>"thread_YYt2zxvmaFfAIHP3ho9nksdm", "run_id"=>nil, "role"=>"user", "content"=>[{"type"=>"text", "text"=>{"value"=>"I would like to visit Kenya", "annotations"=>[]}}], "attachments"=>[], "metadata"=>{}}], "first_id"=>"msg_dmdqthtBgMKCbTPUQ36L3HRL", "last_id"=>"msg_CbA7QR8hdQ96DIQ1FXWbGSRR", "has_more"=>false}
+  end
 
   # def ai_response
   #   client = OpenAI::Client.new
--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,10 +1,50 @@
-import { Controller } from "@hotwired/stimulus"
-
+import { Controller } from "@hotwired/stimulus";
+import { marked } from "marked";
 export default class extends Controller {
-  static targets = ["messages", "input"]
+  static targets = ["messages", "input", "question"]
 
   connect() {
-    // set the heigt of the messages container to the height of the window
+    // set the height of the messages container to the height of the window
     this.messagesTarget.style.height = `${window.innerHeight - 100}px`
+  }
+  
+  submit(event) {
+    event.preventDefault();
+    fetch('/questions.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+      },
+      body: JSON.stringify({ question: {question: this.questionTarget.value } })
+    }).then(response => {
+      if (response.ok) {
+        const form = event.target;
+        const formData = new FormData(form);
+
+        fetch(form.action, {
+          method: form.method,
+          headers: {
+            'Accept': 'application/json',
+            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+          },
+          body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+          // render text with turbo stream
+          const response = data.question.answer;
+          this.afterSubmit();
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+      }
+    })
+  }
+
+  afterSubmit() {
+    this.inputTarget.value = "";
+    this.questionTarget.value = "";
   }
 }
