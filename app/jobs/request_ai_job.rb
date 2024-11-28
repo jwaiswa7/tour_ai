f3439af1b8eb10aeb6130af9ffde1be1253a91d5
Joshua Waiswa <jwaiswa7@gmail.com>
2024-11-28 23:53:43 +0300

    Update the form with tubo stream

--- app/jobs/request_ai_job.rb
+++ app/jobs/request_ai_job.rb
@@ -12,5 +12,12 @@ class RequestAiJob
       partial: "chats/ai_message", # Partial to render
       locals: { message: response } # Pass data to the partial
     )
+
+    Turbo::StreamsChannel.broadcast_replace_to(
+      "#{thread_id}-messages-stream", # Stream name
+      target: "#{thread_id}-chat-form", # Turbo Frame ID
+      partial: "chats/form", # Partial to render
+      locals: { thread_id: thread_id } # Pass data to the partial
+    )
   end
 end
