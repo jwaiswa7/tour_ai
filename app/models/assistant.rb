d667ab799103aba65df2d295096c794b4cbcac11
Joshua Waiswa <jwaiswa7@gmail.com>
2024-11-29 16:25:06 +0300

    Remove redundant models

--- app/models/assistant.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-class Assistant < ApplicationRecord
-
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "description", "id", "instructions", "model", "name", "remote_id", "temperature", "updated_at"]
-  end
-end
--- app/models/vector_store.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-class VectorStore < ApplicationRecord
-end
