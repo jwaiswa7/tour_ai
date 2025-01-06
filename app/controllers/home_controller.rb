75c47afef7f5d84550683b29f0fee7ef69992442
Joshua Waiswa <jwaiswa7@gmail.com>
2025-01-06 08:26:01 +0300

    Clean API service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class HomeController < ApplicationController
   def index
-    @thread_id = Ai::ChatBot::Thread.new.call[:thread_id]
+    @thread_id = Ai::Thread.call[:thread_id]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_08_14_122326) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_11_110833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,7 +40,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_08_14_122326) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'uuid' for column 'id'
-
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 end
