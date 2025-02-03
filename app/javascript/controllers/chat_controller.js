ba820803076885ae1437746851c58a5e957effae
Joshua Waiswa <jwaiswa7@gmail.com>
2025-02-03 18:22:05 +0300

    Stayle chat box

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,16 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["button", "input", "messages", "threadId"]
+  static targets = ["button", "input", "messages", "container"]
 
   connect() {
     this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
   }
 
   handleSubmit(event){
-    console.log('Submitting');
     this.inputTarget.value = "Submitting...";
     this.inputTarget.disabled = true;
     this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
+  }
+
+  toggle() {
+    this.containerTarget.classList.toggle("hidden")
+    // Change the button text
+    if (this.containerTarget.classList.contains("hidden")) {
+      this.buttonTarget.textContent = "Show Chat";
+    } else {
+      this.buttonTarget.textContent = "Hide Chat";
+    }
   }
 }
